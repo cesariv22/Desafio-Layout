f5fb487828907ed0aed6d7b01934080110190b6c
Cesar Riveros <c-riveros@hotmail.com>
2022-05-12 22:59:56 -0400

    challenge finished

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,113 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+  <div>
+    <div>
+      <v-app-bar color="deep-purple accent-4" dense dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>My App</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn>
+          <v-text>Inicio</v-text>
+        </v-btn>
+        <v-btn>
+          <v-text>Buscar</v-text>
+        </v-btn>
+        <v-btn>
+          <v-text>A cerca de</v-text>
+        </v-btn>
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+    </div>
+
+    <PictureCard />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import PictureCard from "./components/PictureCard";
+export default {
+  name: "App",
+  components: {
+    PictureCard,
+  },
+  data() {
+    return {
+      personajes: [
+        {
+          id: 1,
+          nombre: "Morty",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+          status: "Alive",
+          especie: "Human",
+        },
+        {
+          id: 2,
+          nombre: "Rick",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+          status: "Alive",
+          especie: "Human",
+        },
+        {
+          id: 3,
+          nombre: "Summer",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+          status: "Alive",
+          especie: "Human",
+        },
+        {
+          id: 4,
+          nombre: "Beth",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+          status: "Alive",
+          especie: "Human",
+        },
+        {
+          id: 5,
+          nombre: "Jerry",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+          status: "Alive",
+          especie: "Human",
+        },
+        {
+          id: 6,
+          nombre: "Abadango",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+          status: "Alive",
+          especie: "Animal",
+        },
+        {
+          id: 7,
+          nombre: "Abradolf",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
+          status: "Alive",
+          especie: "Human",
+        },
+        {
+          id: 8,
+          nombre: "Adjudicator",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
+          status: "unknown",
+          especie: "Human",
+        },
+        {
+          id: 9,
+          nombre: "Agency",
+          imagen: "https://rickandmortyapi.com/api/character/avatar/9.jpeg",
+          status: "Dead",
+          especie: "Human",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/PictureCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex v-for="item in items" :key="item.id">
+        <v-card>
+          <v-img class="white--text align-end" height="380px" :src="item.image">
+          </v-img>
+          <v-card-title class="pb-0"> {{ item.name }} </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "PictureCard",
+
+  data() {
+    return {
+      items: [
+        {
+          id: "1",
+          name: "Morty Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        },
+        {
+          id: "2",
+          name: "Morty Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        },
+        {
+          id: "3",
+          name: "Summer Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        },
+        {
+          id: "4",
+          name: "Beth Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        },
+        {
+          id: "5",
+          name: "Beth Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        },
+        {
+          id: "6",
+          name: "Jerry Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+        },
+        {
+          id: "7",
+          name: "Abradolf",
+          image: "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
+        },
+        {
+          id: "8",
+          name: "Adjudicator",
+          image: "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
+        },
+        {
+          id: "9",
+          name: "Agency",
+          image: "https://rickandmortyapi.com/api/character/avatar/9.jpeg",
+        },
+      ],
+    };
+  },
+
+  props: {
+    text: String,
+  },
+};
+</script>
+<style scoped>
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
